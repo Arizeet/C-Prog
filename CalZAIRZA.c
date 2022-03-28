a17b467373e7ea5f1bba2f8164b26bffcfe2a1e9
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2022-03-28 12:29:42 +0530

    cyucccccc

--- CalZAIRZA.c
+++ CalZAIRZA.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-#include<math.h>
+#include <math.h>
 float Arith(float num1, float num2, char a)
 {
     switch (a)
@@ -30,7 +30,7 @@ float Linear(float num1, float num2, float num3, int a, float *x_inc, float *y_i
         return (num3 - num2) / num1;
         break;
     case 2:
-        return *x_inc =num3 / num1, *y_inc= num3 / num2;
+        return *x_inc = num3 / num1, *y_inc = num3 / num2;
         break;
     default:
         printf("Invalid input...");
@@ -137,10 +137,10 @@ int main()
 {
     int a;
     int num, num1, num2, num3, num4 = 1;
-    float x, y, z, p, q, D, img1, img2, m=1;
+    float x, y, z, p, q, D;
     char c;
     system ("cls");
-    printf("~~~ CALCULATOR ~~~");
+    printf("****CALCULATOR*****\n\n");
 label_1:
     printf("\n\nEnter the no. against the operation you want:");
     printf("\n\n1. Arithmetic Calculations\n2. Linear equations\n3. Quadratic equation\n4. Factorial\n");
@@ -150,12 +150,11 @@ label_1:
     {
     case 1:
         printf("\nBasic Arithmetic Calculator:\n");
-        scanf("%f%c%f", &x, &c, &y);
-        printf("%.2f %c %.2f = %.2f", x, c, y, Arith(x, y, c));
+        scanf("%d%c%d", &num1, &c, &num2);
+        printf("%.1f %c %.1f = %.2f", (float)num1, c, (float)num2, Arith(num1, num2, c));
         break;
 
     case 2:
-        case2:
         printf("\nLinear equation:\nEnter...\n1. For One Variable\n2. For Two variable\n");
         scanf("%d", &a);
         if (a == 1)
@@ -167,10 +166,9 @@ label_1:
             printf("c=");
             scanf("%f", &z);
             Linear(x, y, z, a, &p, &q);
-            printf("\nThe formed linear eqn is %.2fx + %.2f = %.2f.",x,y,z);
-            printf("\nThe value of x = %.2f", Linear(x, y, z, a, &p, &q));
+            printf("The value of x = %.2f", Linear(x, y, z, a, &p, &q));
         }
-        else if(a==2)
+        else
         {
             printf("Linear eqn in two var is of the form ax + by = c.\nEnter\na=");
             scanf("%f", &x);
@@ -179,13 +177,7 @@ label_1:
             printf("c=");
             scanf("%f", &z);
             Linear(x, y, z, a, &p, &q);
-            printf("\nThe formed linear eqn is %.2fx + %.2f = %.2f.",x,y,z);
-            printf("\nThe x-intercept=%.2f & y-intercept=%.2f.", p, q);
-        }
-        else
-        {
-            printf("\nInvalid input...\n");
-            goto case2;
+            printf("The x-intercept=%.2f & y-intercept=%.2f.", p, q);
         }
         break;
     case 3:
@@ -200,20 +192,17 @@ label_1:
         D = (num2 * num2) - (4 * num1 * num3);
         x = ((-num2) + sqrt(D)) / (2 * num1);
         y = ((-num2) - sqrt(D)) / (2 * num1);
-        img1 = (-num2)/(2*num1);
-        img2 = sqrt(-D)/(2*num1);
         if (D < 0)
         {
-            printf("\nThe root of the formed Quadratic eqn are complex roots.\n");
-            printf("And the roots are %.2f + %.2fi and %.2f - %.2fi",img1,img2,img1,img2);
+            printf("The root of the formed Quadratic eqn are complex roots.");
         }
         else if (D == 0)
         {
-            printf("The roots of the Quadratic eqn are real and equal.\nAnd the root is %.2f", x);
+            printf("The root of the Quadratic eqn is %.2f.", x);
         }
         else if (D > 0)
         {
-            printf("The roots of the Quadratic eqn are real and distinct.\nAnd the roots are %.2f and %.2f.", x, y);
+            printf("The roots are %.2f and %.2f.", x, y);
         }
         break;
     case 4:
@@ -222,74 +211,58 @@ label_1:
         printf("%d! = %d", num, factorial(num));
         break;
     case 5:
-        case5:
-        printf("\nSquare and Cube:\nEnter...\n1. For Square\n2. For Cube\n");
+        printf("Square and Cube:\nEnter...\n1. For Square\n2. For Cube\n");
         scanf("%d", &a);
         if (a == 1)
         {
-            printf("\nEnter the number you want square of:\n");
+            printf("Enter the number you want square of:\n");
             scanf("%f", &x);
             y = x * x;
-            printf("sq(%.2f) = %.2f", x, y);
-        }
-        else if(a==2)
-        {
-            printf("\nEnter the number you want cube of:\n");
-            scanf("%f", &x);
-            y = x * x * x;
-            printf("cube(%.2f) = %.2f", x, y);
+            printf("sq(%f) = %.2f", x, y);
         }
         else
         {
-            printf("\nInvalid input...\n");
-            goto case5;
+            printf("Enter the number you want cube of:\n");
+            scanf("%f", &x);
+            y = x * x * x;
+            printf("sq(%f) = %.2f", x, y);
         }
         break;
     case 6:
-        case6:
         printf("Square-root and Cube-root:\nEnter...\n1. For Square-root\n2. For Cube-root\n");
         scanf("%d", &a);
         if (a == 1)
         {
-            printf("\nEnter the number you want square-root of:\n");
+            printf("Enter the number you want square-root of:\n");
             scanf("%f", &x);
             y = sqrt(x);
-            printf("sqrt(%.2f) = %.2f", x, y);
-        }
-        else if(a==2)
-        {
-            printf("\nEnter the number you want cube-root of:\n");
-            scanf("%f", &x);
-            y = cbrt(x);
-            printf("cbrt(%.2f) = %.2f", x, y);
+            printf("sqrt(%f) = %.2f", x, y);
         }
         else
         {
-            printf("\nInvalid input...\n");
-            goto case6;
+            printf("Enter the number you want cube-root of:\n");
+            scanf("%f", &x);
+            y = cbrt(x);
+            printf("cbrt(%f) = %.2f", x, y);
         }
         break;
     case 7:
         printf("(x)^(y):\nEnter x and y respectively:\nx=");
-        scanf("%d", &num1);
+        scanf("%f", &x);
         printf("y=");
-        scanf("%d", &num2);
-        num3=num2;
-        while (num2>=1)
+        scanf("%d", &num1);
+        for (int i = 1; i <= num1; i++)
         {
-            num4=num4*num1;
-            num2=num2-1;
+            num4 = x * num4;
         }
-        printf("(%d)^(%d) = %d",num1,num3,num4);
+        printf("(%.2f)^(%d) = %.2f", x, num1, (float)num4);
         break;
     case 8:
-        case8:
         printf("Unit Converter:\nEnter...\n1. Length\n2. Area\n3. Volume\n4. Mass\n5. Temperature\n");
         scanf("%d", &num);
         switch (num)
         {
         case 1:
-            case81:
             printf("Convert Length from:\n1. cm to inches\n2. inches to cm\n3. inches to ft\n4. ft to m");
             printf("\n5. km to miles\n6. miles to km\n");
             scanf("%d", &a);
@@ -335,15 +308,8 @@ label_1:
                 Length(x, a);
                 printf("%.2f miles = %.2f km", x, Length(x, a));
             }
-            else
-            {
-                printf("\nInvalid input...\n");
-                goto case81;
-            }
             break;
-            goto case81;
         case 2:
-            case82:
             printf("Convert Area from:\n1. sq(m) to sq(yard)\n2. sq(m) to sq(ft)\n3. Hectare to sq(m)");
             printf("\n4. Acre to sq(m)\n5. Hectare to Acre\n");
             scanf("%d", &a);
@@ -363,14 +329,14 @@ label_1:
             }
             else if (a == 3)
             {
-                printf("Enter area in Hectare: ");
+                printf("Enter area in sq(m): ");
                 scanf("%f", &x);
                 Area(x, a);
                 printf("%.2f Hectare = %.2f sq(m)", x, Area(x, a));
             }
             else if (a == 4)
             {
-                printf("Enter area in Acre: ");
+                printf("Enter area in sq(m): ");
                 scanf("%f", &x);
                 Area(x, a);
                 printf("%.2f Acre = %.2f sq(m)", x, Area(x, a));
@@ -382,14 +348,8 @@ label_1:
                 Area(x, a);
                 printf("%.2f Hectare = %.2f Acre", x, Area(x, a));
             }
-            else
-            {
-                printf("\nInvalid input...\n");
-                goto case82;
-            }
             break;
         case 3:
-            case83:
             printf("Convert Volume from:\n1. cb(m) to l\n2. cb(m) to cb(ft)\n3. cb(ft) to l\n");
             scanf("%d", &a);
             if (a == 1)
@@ -413,14 +373,8 @@ label_1:
                 Volume(x, a);
                 printf("%.2f cb(ft) = %.2f l", x, Volume(x, a));
             }
-            else
-            {
-                printf("\nInvalid input...\n");
-                goto case83;
-            }
             break;
         case 4:
-            case84:
             printf("Convert Mass from\n1. kg to lb\n2. kg to stone\n3. lb to kg\n");
             scanf("%d", &a);
             if (a == 1)
@@ -444,15 +398,9 @@ label_1:
                 Mass(x, a);
                 printf("%.2f lb = %.2f kg", x, Mass(x, a));
             }
-            else
-            {
-                printf("\nInvalid input...\n");
-                goto case84;
-            }
             break;
         case 5:
-            case85:
-            printf("Convert Temperature from\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\n3. Celsius to Kelvin\n");
+            printf("Convert Temperature from\n1. Celsius to Fahrenheit\n2. Fahrenheit\n3. Celsius to Kelvin\n");
             scanf("%d", &a);
             if (a == 1)
             {
@@ -475,22 +423,16 @@ label_1:
                 Temp(x, a);
                 printf("%.2f C = %.2f K", x, Temp(x, a));
             }
-            else
-            {
-                printf("\nInvalid input...\n");
-                goto case85;
-            }
             break;
         default:
-            printf("\nInvalid input...\n");
-            goto case8;
+            printf("Invalid input...");
             break;
         }
         break;
     case 9:
         printf("Body Mass Index:\nEnter...\nYour mass in kg: ");
         scanf("%f", &x);
-        printf("Your height in m: ");
+        printf("\nYour height in m: ");
         scanf("%f", &y);
         D = x / (y * y);
         printf("Your BMI = %.2f", D);
@@ -508,7 +450,6 @@ label_1:
         }
         break;
     case 10:
-        case10:
         printf("Numeral System:\nConvert...\n1. Decimal to Hexadecimal\n2. Decimal to Octal\n");
         scanf("%d",&a);
         if (a==1)
@@ -522,11 +463,6 @@ label_1:
             printf("Enter number in Decimal: ");
             scanf("%d",&num);
             printf("%d (DEC) = %o (OCT)",num,num);
-        }
-        else
-        {
-            printf("\nInvalid input...\n");
-            goto case10;
         }
         break;
     case 11:
