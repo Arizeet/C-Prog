de13e1b837470f83776fe77fd60992579e481508
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2022-05-14 22:59:26 +0530

    jvcu

---
+++ strlen.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    int a=30;
+    system("cls");
+    switch (20.5)
+    {
+        case 2: printf("acsc");
+        case 2: printf("dewc");
+    }
+    printf("MySw");
+    return 0;
+}
