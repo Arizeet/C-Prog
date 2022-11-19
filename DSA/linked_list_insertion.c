b509c9b548b2b9f7ccfd6131737b0f57d6a95797
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2022-11-19 12:34:29 +0530

    knvcnv

--- DSA/linked_list_insertion.c
+++ DSA/linked_list_insertion.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 struct node{
     int data;
     struct node *next;
@@ -39,6 +40,7 @@ void insertB(){
     scanf("%d",&newnode->data);
     newnode->next=head;
     head=newnode;
+    count++;
 }
 void insertE(){
     struct node *newnode, *temp;
@@ -50,18 +52,39 @@ void insertE(){
         temp=temp->next;
     }
     temp->next=newnode;
+    count++;
 }
 void insertSP(){
     int pos,i=1;
     struct node *newnode, *temp;
-    printf("Enter the pos to enter");
+    printf("Enter the pos to enter: ");
     scanf("%d",&pos);
-    if(pos>count){
+    if(pos>count+1){
         printf("Invalid position...");
+    }
+    else if(pos==1){
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter data to insert at beginning: ");
+        scanf("%d",&newnode->data);
+        newnode->next=head;
+        head=newnode;
+        count++;
+    }
+    else if(pos==count+1){
+        struct node *newnode, *temp;
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter data to insert at end: ");
+        scanf("%d",&newnode->data);
+        temp=head;
+        while(temp->next!=0){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+        count++;
     }
     else{
         newnode = (struct node *)malloc(sizeof(struct node));
-        while(i<pos){
+        while(i<=pos){
             temp=head;
             temp=temp->next;
             i++;
@@ -70,21 +93,23 @@ void insertSP(){
         scanf("%d",&newnode->data);
         newnode->next=temp->next;
         temp->next=newnode;
+        count++;
     }
 }
 int main(){
     int ch=1,choice;
     create();
-    printf("LL created: %d nodes in the list\n",count);
+    printf("LL created: %d nodes in the list",count);
     while(ch){
-        printf("\n1. InsertB\t2. InsertE\t3. InsertSP\t4. Display\t5. EXIT\n");
+        printf("\n1. InsertB\t2. InsertE\t3. InsertSP\t4. Display\t5. No. of nodes\t6. EXIT\n");
         scanf("%d",&choice);
         switch(choice){
             case 1: insertB();  break;
             case 2: insertE();  break;
             case 3: insertSP(); break;
             case 4: display();  break;
-            case 5: exit(1);
+            case 5: printf("%d nodes in LL",count); break;
+            case 6: exit(1);
             default:    printf("Invalid i/p...\n");
         }
     }
