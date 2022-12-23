a2fcc6ce9cda9d70a6e77e7cd3c7298448e59e98
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2022-12-24 01:16:14 +0530

    vervcre

--- DSA/bubble_sort_array.c
+++ DSA/bubble_sort_array.c
@@ -3,7 +3,7 @@
 #define N 50
 int main()
 {
-    int a[N],n,temp,c=0;
+    int a[N],n,temp,c=0,pass=0,swap=0;
     system("cls");
     printf("Enter the no of elements in array: ");
     scanf("%d",&n);
@@ -12,8 +12,8 @@ int main()
         printf("Element no. %d: ",i+1);
         scanf("%d",&a[i]);
     }
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n-1; i++)
+    {   c=0;
         for (int j = 0; j < (n-1)-i; j++)
         {
             if (a[j]>a[j+1])
@@ -21,12 +21,18 @@ int main()
                 temp=a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
+                swap++;
+                c=1;
             }
         }
+        if(c==0){
+            break;
+        }
+        pass++;
     }
+    printf("Pass = %d\tSwap = %d\n",pass,swap);
     printf("The sorted array is:\n");
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         printf("%d\t",a[i]);
     }
     return 0;
